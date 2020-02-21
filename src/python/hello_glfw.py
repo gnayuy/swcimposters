fca77c9f6f32f5713619f43007bfcab2ba445990
gnayuy <gnayuy@gmail.com>
2020-02-21 15:31:37 -0800

    add requirements and modify it for python3

--- src/python/hello_glfw.py
+++ src/python/hello_glfw.py
@@ -13,14 +13,14 @@ import sys
 class GlfwExample:
     "Hello world example of OpenGL window using glfw"
     def __init__(self):
-        print "Hello"
+        print("Hello")
         self.renderLoop()
         
     def initGL(self):
         if not glfw.init():
             sys.exit(1)
         self.window = glfw.create_window(640, 480, "Hello", None, None)
-        print self.window
+        print(self.window)
         glfw.make_context_current(self.window)
         glfw.swap_interval(1)
         glfw.set_key_callback(self.window, self.key_callback)
@@ -31,7 +31,7 @@ class GlfwExample:
         glfw.terminate()
         
     def key_callback(self, window, key, scancode, action, mods):
-        print "key_callback"
+        print("key_callback")
         if key == glfw.KEY_ESCAPE and action == glfw.PRESS:
             glfw.set_window_should_close(self.window, True)
 
